b297cbdd35a8536e9ffbd1b6cafd4844e9d22305
moon11yy <subaveron@gmail.com>
2026-07-03 17:13:55 +0300

    PluginInfo.cs done

---
+++ src/PluginInfo.cs
@@ -0,0 +1,10 @@
+namespace LootSearchSpeed;
+
+internal static class PluginInfo
+{
+    public const string Guid = "moon11yy.lootsearchspeed";
+    public const string Name = "moon11yy-LootSearchSpeed";
+    public const string Version = "0.1.0-alpha";
+
+    public const string Author = "moon11yy";
+}
